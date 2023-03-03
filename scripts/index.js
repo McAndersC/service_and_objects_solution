a35445d379646334c9a522f82443f548fb22ebd8
Anders Christensen <anders@medieskolerne.dk>
2023-03-03 12:14:35 +0100

    adjusted detail

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // Vi henter vores service der giver adgang til data
 import service from "./service.js";
 
-// import {productTmpl, productDetailTmpl} from './templates.js';
+import {productTmpl, productDetailTmpl} from './templates.js';
 
 // Referencer til DOM elementer.
 const pageProduct = document.querySelector('.page-product');
@@ -9,23 +9,6 @@ const itemContainer = document.querySelector('.item-container');
 
 // Vi opretter en reference til vores ProductFromFile metode i service.
 let productsFromFile = service.getProductsFromFile();
-
-
-const productTmpl = (product) => `<div>
-    <img src="${product.image}"></img>
-    <h1>${product.title}</h1>
-    <p>${product.description}</p>
-    <span>Author:${product.author} </span>
-    <div><a href="product.html?id=${product.id}">Gå til produkt</a></div>
-</div>`
-
-const productDetailTmpl = (product) => `<div>
-    <img src="${product.image}"></img>
-    <h1>${product.title}</h1>
-    <p>${product.description}</p>
-    <span>Author:${product.author} </span>
-</div>`
-
 
 // Hvis vi har en Item Container så udføre vi følgende arbejde.
 if(itemContainer) {
@@ -53,10 +36,17 @@ if(pageProduct) {
     let productID = new URLSearchParams(search).get('id');
 
     const productContainer = document.querySelector('.product-container');
-    const foundProduct = fileProducts.find( (product) => product.id == productID)
 
-    productContainer.innerHTML = productDetailTmpl(foundProduct);
+    productsFromFile.then(productsInJson => {
 
-    console.log('foundProduct', foundProduct);
+        const foundProduct = productsInJson.find( (product) => product.id == productID)
+
+        productContainer.innerHTML = productDetailTmpl(foundProduct);
+    
+        console.log('foundProduct', foundProduct);
+    
+    })
+    
+
 
 }
